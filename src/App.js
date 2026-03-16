61653c069021c84dc18b9564c71823555b5084c2
Shavali Badvel <55553298+shavalibadvel@users.noreply.github.com>
2026-03-16 20:20:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+//import UserDashboard from "./session1-takehome/UserDashboard";
+import DogPics from "./session2-takehome/DogpPics";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <UserDashboard /> */}
+      <DogPics />
+    </div>
+  );
+}
